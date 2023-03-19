c2d5334e5d09eef08b6fcf24b907e10b195adf18
Luan C. Redmann <luandacostaredmann@gmail.com>
2023-03-19 20:19:38 -0300

    ➕ vue router

--- vueroot/src/App.vue
+++ vueroot/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <BarraMenu/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import BarraMenu from './components/BarraMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    BarraMenu
   }
 }
 </script>
--- vueroot/src/components/BarraMenu.vue
+++ vueroot/src/components/BarraMenu.vue
@@ -5,13 +5,4 @@
 </template>
 
 <style>
-    * {
-        margin: 0px auto;
-        padding: 0px;
-    }
-
-    nav {
-        border-bottom: 1px solid #183624;
-        color: #183624;
-    }
 </style>
